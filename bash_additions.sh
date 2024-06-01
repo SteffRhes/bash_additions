3c7ee755e3b0d0e74ccd1555c27c3d878d2c91a1
SteffRhes <steffrhes@pm.me>
2024-06-01 12:03:30 +0200

    change color of bash info to the left

--- bash_additions.sh
+++ bash_additions.sh
@@ -120,10 +120,10 @@ fi
 
 # commandline status and coloring
 # show only user, no host, and only current folder
-# for normal user (color green):
-export PS1="\[\e[01;32m\]\u:\[\033[01;34m\]\W\[\033[00m\]\$ "
-# for root (color red):
-#export PS1="\[\e[01;31m\]\u:\[\033[01;34m\]\W\[\033[00m\]\$ "
+# for normal user (cyan):
+export PS1="\[\e[30;01;106m\]\u:\[\033[30;01;106m\]\W\[\033[00m\] "
+# for root (red):
+#export PS1="\[\e[30;01;101m\]\u:\[\033[30;01;101m\]\W\[\033[00m\] "
 
 
 # show current folder as terminal title (and show `root` explicitley)
